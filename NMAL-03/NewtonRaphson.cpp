ef61004c574ce9808c918a518a65eb073694d51f
PromitPallab831011 <promitpa;lab273@gmail.com>
2025-07-14 21:40:15 +0600

    code added

---
+++ NMAL-03/NewtonRaphson.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+double f(double x)
+{
+    return pow(x,5)-3*x+5;
+}
+
+double derivative(double x)
+{
+    return 5*pow(x,4)-3;
+}
+int main()
+{
+    return 0;
+}
