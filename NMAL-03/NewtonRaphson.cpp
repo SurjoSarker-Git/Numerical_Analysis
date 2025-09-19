8ae5e2a5fbcaa02c4956864a5038cbfa6bf07af0
PromitPallab831011 <promitpa;lab273@gmail.com>
2025-09-20 01:13:53 +0600

    Task added

--- NMAL-03/NewtonRaphson.cpp
+++ NMAL-03/NewtonRaphson.cpp
@@ -12,5 +12,25 @@ double derivative(double x)
 }
 int main()
 {
+    double x0;
+    cout<<"Enter initial guess:";
+    cin>>x0;
+    double err=1e-3;
+    double x1;
+    int it=0;
+    
+    while(true)
+    {
+        it++;
+        x1=x0-f(x0)/derivative(x0);
+        double error=fabs((x1-x0)/x1);
+        cout<<"Iteration "<<it<<": x="<<x1<<", f(x)="<<f(x1)<<", error="<<error<<endl;
+        if(error<err)
+        {
+            break;
+        }
+        x0=x1;
+    }
+    cout<<"\nApproximate root="<<x1<<endl;
     return 0;
 }
